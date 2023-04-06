838d6682e38b9036adc9479f811c059650865202
Chema <chema@debian-BULLSEYE-live-builder-AMD64>
2023-04-06 13:29:28 +0200

    Primer commit

--- ea20/ea20.java
+++ ea20/ea20.java
@@ -7,28 +7,29 @@ public class ea20 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		FileOutputStream fos = new FileOutputStream("./src/ea20/ea20.bin");
+		FileOutputStream fos = new FileOutputStream("./src/tema7/ea20.bin");
 		BufferedOutputStream bos = new BufferedOutputStream(fos);
 		DataOutputStream dos = new DataOutputStream(bos);
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Escribe tu nombre: ");
 		dos.writeUTF(sc.nextLine());
+		dos.writeUTF("\n");
 		System.out.println("Escribe tu edad: ");
-		dos.writeUTF(sc.nextLine());
+		dos.write(sc.nextInt());
 		dos.close();
 		dos.close();
 		fos.close();
-		
-		InputStream fis = new FileInputStream(new File("./src/ea20/ea20.bin"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-		String linea = "";
-		while((br.readLine()) != null) {
-			System.out.println(linea);
+
+		FileInputStream fis = new FileInputStream("ea20.bin");
+		DataInputStream dis = new DataInputStream(new BufferedInputStream(fis));
+		boolean entrada;
+		while(entrada = !dis.equals(null)) {
+			String nombre = dis.readUTF();
+			System.out.println(nombre);
+			
+			int edad = dis.readInt();
+			System.out.println(edad);
 		}
-		
-		
 	}
-
 }
-
